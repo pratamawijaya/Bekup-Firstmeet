1bacb0fa22c85a3a8a62e05662676a1f4dfe1cb0
pratamawijaya <set.mnemonix@gmail.com>
2016-06-16 17:24:44 +0700

    add :

--- app/src/main/java/com/pratamawijaya/firstmeet/LayoutingActivity.java
+++ app/src/main/java/com/pratamawijaya/firstmeet/LayoutingActivity.java
@@ -2,11 +2,43 @@ package com.pratamawijaya.firstmeet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LayoutingActivity extends AppCompatActivity {
+
+  private static final String TAG = "LayoutingActivity";
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_layouting_linearlayout);
+
+    Log.d(TAG, "onCreate: ");
+  }
+
+  @Override protected void onStart() {
+    super.onStart();
+    Log.d(TAG, "onStart: ");
+    // turn on location detection
+  }
+
+  @Override protected void onResume() {
+    super.onResume();
+    Log.d(TAG, "onResume: ");
+  }
+
+  @Override protected void onStop() {
+    super.onStop();
+    Log.d(TAG, "onStop: ");
+    // turn of location detection
+  }
+
+  @Override protected void onPause() {
+    super.onPause();
+    Log.d(TAG, "onPause: ");
+  }
+
+  @Override protected void onDestroy() {
+    super.onDestroy();
+    Log.d(TAG, "onDestroy: ");
   }
 }
---
+++ app/src/main/java/com/pratamawijaya/firstmeet/UserActivity.java
@@ -0,0 +1,12 @@
+package com.pratamawijaya.firstmeet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_user);
+  }
+}
